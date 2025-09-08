3d3c5b3a133cc5042d6cde90cc4e20375e95d211
melosh101 <git@milasholsting.dk>
2025-09-08 09:49:03 +0200

    start running with fastify cli

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,66 +1,54 @@
 import { db } from "@webdev/db";
 import env from "@webdev/db/env";
-import Fastify from "fastify";	
+import Fastify, { type FastifyPluginAsync } from "fastify";
 import { hilsnerTable } from "@webdev/db/schema";
 import z from "zod";
-import cors from "@fastify/cors"
-
-const fastify = Fastify({
-	logger: true,
-});
-
-if(env.NODE_ENV !== "production") {
-	await fastify.register(cors, {
-		origin: ["*"],
-	});
-}
-
+import cors from "@fastify/cors";
 
 const insertHilsen = z.object({
 	name: z.string().min(2).max(100),
 	message: z.string().min(30).max(500),
 });
 
-fastify.get("/hilsen", async (_request, reply) => {
-	const hilsner = await db.select().from(hilsnerTable);
-	reply.send(hilsner);
-});
-
-fastify.post("/hilsen", async (request, reply) => {
-	const parseResult = insertHilsen.safeParse(request.body);
-	if (!parseResult.success) {
-		reply.status(400).send({
-			status: 400,
-			message: "Bad request",
-			...z.treeifyError(parseResult.error),
-		});
-		return;
-	}
-	await db.insert(hilsnerTable).values({
-		...parseResult.data,
+const app: FastifyPluginAsync<{}> = async (fastify, opts): Promise<void> => {
+	// place here any custom plugins or routes
+	await fastify.register(cors, {
+		origin: ["*"],
 	});
-	reply.status(201).send(parseResult.data);
-});
-fastify.route({
-	method: ["get", "post", "put", "delete", "patch"],
-	url: "/*",
-	handler: async (request, reply) => {
-		fastify.log.info(`${request.ip} tried to access ${request.method} ${request.url}`);
-		reply.status(404).send({
-			status: 404,
-			message: `No route on ${request.method} ${request.url}`,
-		});
-	}
-});
 
-fastify.listen(
-	{
-		port: Number(env.PORT || 3000),
-	},
-	(err) => {
-		if (err) {
-			fastify.log.error(err);
-			return process.exit(1);
+	fastify.get("/hilsen", async (_request, reply) => {
+		const hilsner = await db.select().from(hilsnerTable);
+		reply.send(hilsner);
+	});
+
+	fastify.post("/hilsen", async (request, reply) => {
+		const parseResult = insertHilsen.safeParse(request.body);
+		if (!parseResult.success) {
+			reply.status(400).send({
+				status: 400,
+				message: "Bad request",
+				...z.treeifyError(parseResult.error),
+			});
+			return;
 		}
-	},
-);
+		await db.insert(hilsnerTable).values({
+			...parseResult.data,
+		});
+		reply.status(201).send(parseResult.data);
+	});
+	fastify.route({
+		method: ["get", "post", "put", "delete", "patch"],
+		url: "/*",
+		handler: async (request, reply) => {
+			fastify.log.info(
+				`${request.ip} tried to access ${request.method} ${request.url}`,
+			);
+			reply.status(404).send({
+				status: 404,
+				message: `No route on ${request.method} ${request.url}`,
+			});
+		},
+	});
+};
+
+export default app;
