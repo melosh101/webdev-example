73af6c69e61de8b98d3633c71751eaae28548d09
melosh101 <git@milasholsting.dk>
2025-09-18 16:54:58 +0200

    remove catch all

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -35,19 +35,6 @@ const app: FastifyPluginAsync<{}> = async (fastify, opts): Promise<void> => {
 		});
 		reply.status(201).send(parseResult.data);
 	});
-	fastify.route({
-		method: ["get", "post", "put", "delete", "patch"],
-		url: "/*",
-		handler: async (request, reply) => {
-			fastify.log.info(
-				`${request.ip} tried to access ${request.method} ${request.url}`,
-			);
-			reply.status(404).send({
-				status: 404,
-				message: `No route on ${request.method} ${request.url}`,
-			});
-		},
-	});
 };
 
 export default app;
